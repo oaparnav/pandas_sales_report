ff08b30a69d2fbe1bab0b6e701bf55ca7a75db09
lt96mr <eeswar.krishnavarjula@ing.com>
2023-06-28 20:53:17 +0200

    fix: update new files

--- access_database.py
+++ access_database.py
@@ -1,12 +1,9 @@
 import sqlite3
 
 def set_data_to_db(data):
-    connection = sqlite3.connect('sales.db')
-    if connection:
+    with sqlite3.connect('sales.db') as connection:
         print("Database connection successful")
-        # Connect to the database
         cursor = connection.cursor()
-        # Create the table
         cursor.execute('''
             CREATE TABLE IF NOT EXISTS salestable (
                 product_name TEXT,
@@ -15,51 +12,25 @@ def set_data_to_db(data):
                 date_of_sale TEXT
             )
         ''')
-        
-        # Clear existing data from the table
         cursor.execute("DELETE FROM salestable")
-        
-        for item in data:
-            cursor.execute('''
-                INSERT INTO salestable (product_name, quantity_sold, price, date_of_sale)
-                VALUES (?, ?, ?, ?)
-            ''', (item['product_name'], item['quantity_sold'], item['price'], item['date_of_sale']))
-
-        # Commit the changes and close the connection
+        cursor.executemany('''
+            INSERT INTO salestable (product_name, quantity_sold, price, date_of_sale)
+            VALUES (?, ?, ?, ?)
+        ''', [(item['product_name'], item['quantity_sold'], item['price'], item['date_of_sale']) for item in data])
         connection.commit()
-        connection.close()
-    else:
-        print('Error!!')
 
-def set_processed_data_to_db(dbname, table_name, data, key1, key2):
-    connection = sqlite3.connect('total_sales.db')
-    if connection:
+def set_processed_data_to_db(dbname, table_name, data):
+    with sqlite3.connect(f'{dbname}.db') as connection:
         cursor = connection.cursor()
-        # Create the table
-        cursor.execute(
-            f"CREATE TABLE IF NOT EXISTS {table_name} ({key1} REAL, {key2} REAL)")
-        
-        # Clear existing data from the table
+        cursor.execute(f"CREATE TABLE IF NOT EXISTS {table_name} (key REAL, value REAL)")
         cursor.execute(f"DELETE FROM {table_name}")
-        
-        # Insert data into the table
-        for key1, key2 in data.items():
-            cursor.execute(
-                f"INSERT INTO {table_name} VALUES (?, ?)", (key1, key2))
-        # Commit the changes and close the connection
+        cursor.executemany(f"INSERT INTO {table_name} VALUES (?, ?)", data.items())
         connection.commit()
-        connection.close()
-    else:
-        print('Error setting individual data!!')
-    
+
 def retrieve_info_from_table(dbname, table_name):
-    connection = sqlite3.connect(f'{dbname}.db')
-    if connection:
-        # Connect to the database
+    with sqlite3.connect(f'{dbname}.db') as connection:
         cursor = connection.cursor()
         cursor.execute(f"SELECT * FROM {table_name}")
         total_sales_data = cursor.fetchall()
         print(f"{table_name}: ", total_sales_data)
         return total_sales_data
-    else:
-        print("Unable to receive info at this moment")
--- app.py
+++ app.py
@@ -7,124 +7,88 @@ from access_database import set_data_to_db, retrieve_info_from_table, set_proces
 
 class Sales:
 
-    ## intialization of variables
+    # intialization of variables
     def __init__(self):
         self.sales_data = []
         self.total_prices = {}
         self.total_prices_per_year = {}
         self.average_prices = {}
         self.sales_distribution = {}
-    
-    ## get sales data
+
+    # get sales data
     def get_sales_data(self, use_cols=["product_name", "quantity_sold", "price", "date_of_sale"]):
         self.sales_data = pd.read_csv('sales_data.csv', usecols=use_cols)
         self.sales_data = self.sales_data.dropna()
         self.data_dict = self.sales_data.to_dict(orient='records')
         set_data_to_db(self.data_dict)
-    
-    ## draw plot charts for each data
+
+    # draw plot charts for each data
     def drawPlotChart(self):
-        # Create a PDF file
         pdf_file = 'combined_graphs.pdf'
         pdf_pages = PdfPages(pdf_file)
 
-        # Create a figure and subplots
         fig, axes = plt.subplots(2, 2, figsize=(12, 8))
 
-        # Total Sales per Product
-        if self.total_prices:
-            total_prices = pd.DataFrame({'Product': list(
-                self.total_prices.keys()), 'Sales': list(self.total_prices.values())})
-            ax1 = axes[0, 0]
-            total_prices.plot.barh(
-                x='Product', y='Sales', rot=0, color='blue', ax=ax1)
-            ax1.set_ylabel('Products')
-            ax1.set_xlabel('Total sales')
-            ax1.set_title('Total Sales per Product')
+        plots = [
+            (list(self.total_prices.keys()), list(self.total_prices.values()),
+             'Product', 'Total sales', 'Total Sales per Product', 'blue'),
+            (list(self.total_prices_per_year.keys()), list(self.total_prices_per_year.values(
+            )), 'Year', 'Total sales', 'Total Sales per Year', 'green'),
+            (list(self.average_prices.keys()), list(self.average_prices.values()),
+             'Product', 'Total Average Prices', 'Average Price per Product', 'red'),
+            (list(self.sales_distribution.keys()), list(self.sales_distribution.values(
+            )), 'Product', 'Quantity', 'Sales Distribution by Quantity', 'orange')
+        ]
 
-        # Total Sales per Year
-        if self.total_prices_per_year:
-            plotLine = pd.DataFrame({'Year': list(self.total_prices_per_year.keys(
-            )), 'Sales': list(self.total_prices_per_year.values())})
-            ax2 = axes[0, 1]
-            plotLine.plot.line(x='Year', y='Sales', rot=0,
-                               color='green', ax=ax2)
-            ax2.set_ylabel('Total sales')
-            ax2.set_xlabel('Year')
-            ax2.set_title('Total Sales per Year')
+        for i, (x, y, x_label, y_label, title, color) in enumerate(plots):
+            ax = axes[i // 2, i % 2]
+            plot_data = pd.DataFrame({x_label: x, y_label: y})
+            plot_data.plot.line(x=x_label, y=y_label,
+                                rot=90, color=color, ax=ax)
+            ax.set_ylabel(y_label)
+            ax.set_xlabel(x_label)
+            ax.set_title(title)
 
-        # Average Price per Product
-        if self.average_prices:
-            plotLine = pd.DataFrame({'Product': list(
-                self.average_prices.keys()), 'Sales': list(self.average_prices.values())})
-            ax3 = axes[1, 0]
-            plotLine.plot.line(x='Product', y='Sales',
-                               rot=90, color='red', ax=ax3)
-            ax3.set_ylabel('Total Average Prices')
-            ax3.set_xlabel('Products')
-            ax3.set_title('Average Price per Product')
-
-        # Sales Distribution by Quantity
-        if self.sales_distribution:
-            plotLine = pd.DataFrame({'Product': list(
-                self.sales_distribution.keys()), 'Sales': list(self.sales_distribution.values())})
-            ax4 = axes[1, 1]
-            plotLine.plot.line(x='Product', y='Sales',
-                               rot=90, color='orange', ax=ax4)
-            ax4.set_ylabel('Quantity')
-            ax4.set_xlabel('Products')
-            ax4.set_title('Sales Distribution by Quantity')
-
-        # Adjust the spacing between subplots
         plt.tight_layout()
-
-        # Save the combined chart as PDF
         pdf_pages.savefig(fig)
-
-        # Close the PDF file
         pdf_pages.close()
-
-        # Display the file path
         print("Combined graphs saved as PDF:", pdf_file)
 
-    ## create a data set for different graphs
+    # create a data set for different graphs
     def generic_product_set(self):
         product_set = retrieve_info_from_table('sales', 'salestable')
-        total_prices = {}
-        total_quantities = {}
-        for item in product_set:
-            name = item[0]
-            price = item[1]
-            quantity = item[2]
-            year = pd.to_datetime(str(item[3])).year
+        for name, price, quantity, year in [(item[0], item[1], item[2], pd.to_datetime(str(item[3])).year) for item in product_set]:
             total = price * quantity
-
-            total_prices[name] = total_prices.get(name, 0) + total
-            total_quantities[name] = total_quantities.get(name, 0) + quantity
             self.total_prices[name] = self.total_prices.get(name, 0) + total
             self.total_prices_per_year[year] = self.total_prices_per_year.get(
                 year, 0) + total
             self.sales_distribution[name] = self.sales_distribution.get(
                 name, []) + [quantity]
-        for name, quantities in self.sales_distribution.items():
-            self.sales_distribution[name] = sum(quantities)
+        self.sales_distribution = {
+            name: sum(quantities) for name, quantities in self.sales_distribution.items()}
         self.average_prices = {
-            name: total_prices[name] / total_quantities[name] for name in total_prices}
+            name: self.total_prices[name] / self.sales_distribution[name] for name in self.total_prices}
         self.drawPlotChart()
 
-    ## store processd information to db by creating tables
+    # store processd information to db by creating tables
     def set_processed_data_to_db(self):
-        set_processed_data_to_db('total_sales', 'products', self.total_prices, 'name', 'price')
-        set_processed_data_to_db('total_sales', 'years', self.total_prices_per_year, 'year', 'price')
-        set_processed_data_to_db('total_sales', 'average', self.average_prices, 'name', 'price')
-        set_processed_data_to_db('total_sales', 'quantity', self.sales_distribution, 'name', 'quantity')
-    
-    ## reteieve the processed info from table if required(testing purpose)
+        data_to_store = {
+            'products': self.total_prices,
+            'years': self.total_prices_per_year,
+            'average': self.average_prices,
+            'quantity': self.sales_distribution
+        }
+
+        for table_name, data in data_to_store.items():
+            set_processed_data_to_db('total_sales', table_name, data)
+
+    # reteieve the processed info from table if required(testing purpose)
     def retrieve_info_from_table_data(self):
-        retrieve_info_from_table('total_sales', 'products')
-        retrieve_info_from_table('total_sales', 'years')
-        retrieve_info_from_table('total_sales', 'average')
-        retrieve_info_from_table('total_sales', 'quantity')
+        tables_to_retrieve = ['products', 'years', 'average', 'quantity']
+
+        for table_name in tables_to_retrieve:
+            retrieve_info_from_table('total_sales', table_name)
+
 
 sales_info = Sales()
 sales_info.get_sales_data()
