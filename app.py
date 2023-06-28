865b9da45b4c4c73971680fb1530884f59a80162
lt96mr <eeswar.krishnavarjula@ing.com>
2023-06-28 21:32:45 +0200

    fix: update app.py

--- app.py
+++ app.py
@@ -1,60 +1,55 @@
-# -*- coding: utf-8 -*-
-
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 from access_database import set_data_to_db, retrieve_info_from_table, set_processed_data_to_db
 
+
 class Sales:
 
     # intialization of variables
     def __init__(self):
-        self.sales_data = []
+        self.sales_data = pd.DataFrame()
         self.total_prices = {}
         self.total_prices_per_year = {}
         self.average_prices = {}
         self.sales_distribution = {}
-
-    # get sales data
+        
+    # get sales info from csv and update to db
     def get_sales_data(self, use_cols=["product_name", "quantity_sold", "price", "date_of_sale"]):
-        self.sales_data = pd.read_csv('sales_data.csv', usecols=use_cols)
-        self.sales_data = self.sales_data.dropna()
-        self.data_dict = self.sales_data.to_dict(orient='records')
-        set_data_to_db(self.data_dict)
+        self.sales_data = pd.read_csv(
+            'sales_data.csv', usecols=use_cols).dropna()
+        set_data_to_db(self.sales_data.to_dict(orient='records'))
 
     # draw plot charts for each data
     def drawPlotChart(self):
         pdf_file = 'combined_graphs.pdf'
         pdf_pages = PdfPages(pdf_file)
-
         fig, axes = plt.subplots(2, 2, figsize=(12, 8))
-
         plots = [
-            (list(self.total_prices.keys()), list(self.total_prices.values()),
-             'Product', 'Total sales', 'Total Sales per Product', 'blue'),
-            (list(self.total_prices_per_year.keys()), list(self.total_prices_per_year.values(
-            )), 'Year', 'Total sales', 'Total Sales per Year', 'green'),
-            (list(self.average_prices.keys()), list(self.average_prices.values()),
-             'Product', 'Total Average Prices', 'Average Price per Product', 'red'),
-            (list(self.sales_distribution.keys()), list(self.sales_distribution.values(
-            )), 'Product', 'Quantity', 'Sales Distribution by Quantity', 'orange')
+            (self.total_prices, 'Product', 'Total sales',
+             'Total Sales per Product', 'blue'),
+            (self.total_prices_per_year, 'Year',
+             'Total sales', 'Total Sales per Year', 'green'),
+            (self.average_prices, 'Product', 'Total Average Prices',
+             'Average Price per Product', 'red'),
+            (self.sales_distribution, 'Product', 'Quantity',
+             'Sales Distribution by Quantity', 'orange')
         ]
-
-        for i, (x, y, x_label, y_label, title, color) in enumerate(plots):
+        for i, (data, x_label, y_label, title, color) in enumerate(plots):
             ax = axes[i // 2, i % 2]
-            plot_data = pd.DataFrame({x_label: x, y_label: y})
+            plot_data = pd.DataFrame(
+                {x_label: data.keys(), y_label: data.values()})
             plot_data.plot.line(x=x_label, y=y_label,
                                 rot=90, color=color, ax=ax)
             ax.set_ylabel(y_label)
             ax.set_xlabel(x_label)
             ax.set_title(title)
-
         plt.tight_layout()
         pdf_pages.savefig(fig)
         pdf_pages.close()
         print("Combined graphs saved as PDF:", pdf_file)
 
-    # create a data set for different graphs
+    # create a data set from database to generate graphs
     def generic_product_set(self):
         product_set = retrieve_info_from_table('sales', 'salestable')
         for name, price, quantity, year in [(item[0], item[1], item[2], pd.to_datetime(str(item[3])).year) for item in product_set]:
@@ -78,14 +73,12 @@ class Sales:
             'average': self.average_prices,
             'quantity': self.sales_distribution
         }
-
         for table_name, data in data_to_store.items():
             set_processed_data_to_db('total_sales', table_name, data)
 
     # reteieve the processed info from table if required(testing purpose)
     def retrieve_info_from_table_data(self):
         tables_to_retrieve = ['products', 'years', 'average', 'quantity']
-
         for table_name in tables_to_retrieve:
             retrieve_info_from_table('total_sales', table_name)
 
