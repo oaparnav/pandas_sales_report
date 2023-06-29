431b789b9d2394799f818602573000d2977aaf0c
lt96mr <eeswar.krishnavarjula@ing.com>
2023-06-29 09:03:11 +0200

    fix: update logging

--- access_database.py
+++ access_database.py
@@ -1,36 +1,56 @@
 import sqlite3
+import logging
 
 def set_data_to_db(data):
-    with sqlite3.connect('sales.db') as connection:
-        print("Database connection successful")
-        cursor = connection.cursor()
-        cursor.execute('''
-            CREATE TABLE IF NOT EXISTS salestable (
-                product_name TEXT,
-                quantity_sold REAL,
-                price REAL,
-                date_of_sale TEXT
-            )
-        ''')
-        cursor.execute("DELETE FROM salestable")
-        cursor.executemany('''
-            INSERT INTO salestable (product_name, quantity_sold, price, date_of_sale)
-            VALUES (?, ?, ?, ?)
-        ''', [(item['product_name'], item['quantity_sold'], item['price'], item['date_of_sale']) for item in data])
-        connection.commit()
+    try:
+        with sqlite3.connect('sales.db') as connection:
+            print("Database connection successful")
+            cursor = connection.cursor()
+            cursor.execute('''
+                CREATE TABLE IF NOT EXISTS salestable (
+                    product_name TEXT,
+                    quantity_sold REAL,
+                    price REAL,
+                    date_of_sale TEXT
+                )
+            ''')
+            cursor.execute("DELETE FROM salestable")
+            cursor.executemany('''
+                INSERT INTO salestable (product_name, quantity_sold, price, date_of_sale)
+                VALUES (?, ?, ?, ?)
+            ''', [(item['product_name'], item['quantity_sold'], item['price'], item['date_of_sale']) for item in data])
+            connection.commit()
+        logging.info("Successfully stored the sales data into database")
+    except Exception as e:
+        logging.error(
+            "An error occurred while setting data to the database:", str(e))
 
 def set_processed_data_to_db(dbname, table_name, data):
-    with sqlite3.connect(f'{dbname}.db') as connection:
-        cursor = connection.cursor()
-        cursor.execute(f"CREATE TABLE IF NOT EXISTS {table_name} (key REAL, value REAL)")
-        cursor.execute(f"DELETE FROM {table_name}")
-        cursor.executemany(f"INSERT INTO {table_name} VALUES (?, ?)", data.items())
-        connection.commit()
+    try:
+        with sqlite3.connect(f'{dbname}.db') as connection:
+            cursor = connection.cursor()
+            cursor.execute(
+                f"CREATE TABLE IF NOT EXISTS {table_name} (key REAL, value REAL)")
+            cursor.execute(f"DELETE FROM {table_name}")
+            cursor.executemany(
+                f"INSERT INTO {table_name} VALUES (?, ?)", data.items())
+            connection.commit()
+        logging.info(
+            f"Successfully stored the processed data of {table_name} into database")
+    except Exception as e:
+        logging.error(
+            "An error occurred while storing processed data:", str(e))
 
 def retrieve_info_from_table(dbname, table_name):
-    with sqlite3.connect(f'{dbname}.db') as connection:
-        cursor = connection.cursor()
-        cursor.execute(f"SELECT * FROM {table_name}")
-        total_sales_data = cursor.fetchall()
-        print(f"{table_name}: ", total_sales_data)
-        return total_sales_data
+    try:
+        with sqlite3.connect(f'{dbname}.db') as connection:
+            cursor = connection.cursor()
+            cursor.execute(f"SELECT * FROM {table_name}")
+            total_sales_data = cursor.fetchall()
+            print(f"{table_name}: ", total_sales_data)
+            logging.info(
+                f"Successfully retrieved the data of {table_name} from database")
+            return total_sales_data
+    except Exception as e:
+        logging.error(
+            "An error occurred while retrieving information from the table:", str(e))
--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import logging
 from matplotlib.backends.backend_pdf import PdfPages
 from access_database import set_data_to_db, retrieve_info_from_table, set_processed_data_to_db
+
+# Configure logging
+logging.basicConfig(filename='audit.log', level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 class Sales:
@@ -9,82 +14,113 @@ class Sales:
     # intialization of variables
     def __init__(self):
         self.sales_data = pd.DataFrame()
-        self.total_prices = {}
+        self.total_prices_per_product = {}
         self.total_prices_per_year = {}
         self.average_prices = {}
         self.sales_distribution = {}
 
     # get sales info from csv and update to db
-    def get_sales_data(self, use_cols=["product_name", "quantity_sold", "price", "date_of_sale"]):
-        self.sales_data = pd.read_csv(
-            'sales_data.csv', usecols=use_cols).dropna()
-        set_data_to_db(self.sales_data.to_dict(orient='records'))
+    def get_sales_data(self, csv_file='sales_data.csv'):
+        try:
+            logging.info(
+                "Initiating the process of sales data from csv file to store in the database.")
+            self.sales_data = pd.read_csv(csv_file).dropna()
+            set_data_to_db(self.sales_data.to_dict(orient='records'))
+        except FileNotFoundError:
+            logging.error("Error: sales_data.csv file not found.")
+        except Exception as e:
+            logging.error(
+                "An error occurred while getting sales data: %s", str(e))
 
     # create a data set from database to generate graphs
     def generic_product_set(self):
-        product_set = retrieve_info_from_table('sales', 'salestable')
-        for name, price, quantity, year in [(item[0], item[1], item[2], pd.to_datetime(str(item[3])).year) for item in product_set]:
-            total = price * quantity
-            self.total_prices[name] = self.total_prices.get(name, 0) + total
-            self.total_prices_per_year[year] = self.total_prices_per_year.get(
-                year, 0) + total
-            self.sales_distribution[name] = self.sales_distribution.get(
-                name, []) + [quantity]
-        self.sales_distribution = {
-            name: sum(quantities) for name, quantities in self.sales_distribution.items()}
-        self.average_prices = {
-            name: self.total_prices[name] / self.sales_distribution[name] for name in self.total_prices}
-        self.drawPlotChart()
+        try:
+            product_set = retrieve_info_from_table('sales', 'salestable')
+            for name, price, quantity, year in [(item[0], item[1], item[2], pd.to_datetime(str(item[3])).year) for item in product_set]:
+                total = price * quantity
+                self.total_prices_per_product[name] = self.total_prices_per_product.get(
+                    name, 0) + total
+                self.total_prices_per_year[year] = self.total_prices_per_year.get(
+                    year, 0) + total
+                self.sales_distribution[name] = self.sales_distribution.get(
+                    name, []) + [quantity]
+            self.sales_distribution = {
+                name: sum(quantities) for name, quantities in self.sales_distribution.items()}
+            self.average_prices = {
+                name: self.total_prices_per_product[name] / self.sales_distribution[name] for name in self.total_prices_per_product}
+            logging.info("Sales processed data set created succesfully.")
+            self.drawPlotChart()
+        except Exception as e:
+            logging.error(
+                "An error occurred while generating the data set:", str(e))
 
     # draw plot charts for each data
     def drawPlotChart(self):
-        pdf_file = 'sales_report.pdf'
-        pdf_pages = PdfPages(pdf_file)
-        fig, axes = plt.subplots(2, 2, figsize=(12, 8))
-        plots = [
-            (self.total_prices, 'Product', 'Total sales',
-             'Total Sales per Product', 'blue'),
-            (self.total_prices_per_year, 'Year',
-             'Total sales', 'Total Sales per Year', 'green'),
-            (self.average_prices, 'Product', 'Total Average Prices',
-             'Average Price per Product', 'red'),
-            (self.sales_distribution, 'Product', 'Quantity',
-             'Sales Distribution by Quantity', 'orange')
-        ]
-        for i, (data, x_label, y_label, title, color) in enumerate(plots):
-            ax = axes[i // 2, i % 2]
-            plot_data = pd.DataFrame(
-                {x_label: data.keys(), y_label: data.values()})
-            plot_data.plot.line(x=x_label, y=y_label,
-                                rot=90, color=color, ax=ax)
-            ax.set_ylabel(y_label)
-            ax.set_xlabel(x_label)
-            ax.set_title(title)
-        plt.tight_layout()
-        pdf_pages.savefig(fig)
-        pdf_pages.close()
-        print("Combined graphs saved as PDF:", pdf_file)
+        try:
+            logging.info("Initiated process to generate sales report")
+            pdf_file = 'sales_report.pdf'
+            pdf_pages = PdfPages(pdf_file)
+            fig, axes = plt.subplots(2, 2, figsize=(12, 8))
+            plots = [
+                (self.total_prices_per_product, 'Product', 'Total sales',
+                 'Total Sales per Product', 'blue'),
+                (self.total_prices_per_year, 'Year',
+                 'Total sales', 'Total Sales per Year', 'green'),
+                (self.average_prices, 'Product', 'Total Average Prices',
+                 'Average Price per Product', 'red'),
+                (self.sales_distribution, 'Product', 'Quantity',
+                 'Sales Distribution by Quantity', 'orange')
+            ]
+            for i, (data, x_label, y_label, title, color) in enumerate(plots):
+                ax = axes[i // 2, i % 2]
+                plot_data = pd.DataFrame(
+                    {x_label: data.keys(), y_label: data.values()})
+                plot_data.plot.line(x=x_label, y=y_label,
+                                    rot=90, color=color, ax=ax)
+                ax.set_ylabel(y_label)
+                ax.set_xlabel(x_label)
+                ax.set_title(title)
+            plt.tight_layout()
+            pdf_pages.savefig(fig)
+            pdf_pages.close()
+            logging.info("Successfully generated sales report as pdf")
+        except Exception as e:
+            logging.error(
+                "An error occurred while drawing plot charts:", str(e))
 
     # store processed information to db by creating tables
-    def set_processed_data_to_db(self):
-        data_to_store = {
-            'products': self.total_prices,
-            'years': self.total_prices_per_year,
-            'average': self.average_prices,
-            'quantity': self.sales_distribution
-        }
-        for table_name, data in data_to_store.items():
-            set_processed_data_to_db('total_sales', table_name, data)
+    def set_processed_info_to_db(self, dbname='total_sales', table_prefix=''):
+        try:
+            logging.info(
+                "Initiated the process of storing the processed data to db")
+            data_to_store = {
+                'products': self.total_prices_per_product,
+                'years': self.total_prices_per_year,
+                'average': self.average_prices,
+                'quantity': self.sales_distribution
+            }
+            for table_name, data in data_to_store.items():
+                set_processed_data_to_db(
+                    dbname, f"{table_prefix}{table_name}", data)
+        except Exception as e:
+            logging.error(
+                "An error occurred while intiating of storing processed data:", str(e))
 
     # reteieve the processed info from table if required(testing purpose)
-    def retrieve_info_from_table_data(self):
-        tables_to_retrieve = ['products', 'years', 'average', 'quantity']
-        for table_name in tables_to_retrieve:
-            retrieve_info_from_table('total_sales', table_name)
+    def retrieve_info_from_table_data(self, dbname='total_sales', table_prefix=''):
+        try:
+            logging.info(
+                "Successfully initiated the process of retrieving information")
+            tables_to_retrieve = ['products', 'years', 'average', 'quantity']
+            for table_name in tables_to_retrieve:
+                retrieve_info_from_table(dbname, f"{table_prefix}{table_name}")
+        except Exception as e:
+            logging.error(
+                "An error occurred while process of intiating retrieving information from the table:", str(e))
 
 
 sales_info = Sales()
 sales_info.get_sales_data()
 sales_info.generic_product_set()
-sales_info.set_processed_data_to_db()
+sales_info.set_processed_info_to_db()
 sales_info.retrieve_info_from_table_data()
